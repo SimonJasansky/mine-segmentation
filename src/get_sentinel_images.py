baf991ce96f7d20db2bf7f0854c6a486472dd078
Simon Jasansky <64031597+SimonJasansky@users.noreply.github.com>
2024-05-15 16:41:20 +0000

    feat: add stackstac methods to ReadSTAC class

--- src/get_sentinel_images.py
+++ src/get_sentinel_images.py
@@ -1,24 +1,22 @@
 import math
-import requests
-import subprocess
-import numpy as np
-import leafmap
 import planetary_computer
-import geopandas
-import rich.table
 import pystac_client
+import stackstac
+import xarray
+import pyproj
+import rioxarray
 import rasterio
+import requests
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+from osgeo import gdal
 
 
-from osgeo import gdal, osr
-from pyproj import Transformer, CRS
-from pathlib import Path
+from pyproj import Transformer
 from shapely.geometry import Point, mapping, box
 from pystac.extensions.eo import EOExtension as eo
-from IPython.display import Image, display
-from PIL import Image
-from rasterio import windows, features, warp
-
 
 class ReadSTAC():
     """
@@ -41,18 +39,78 @@ class ReadSTAC():
         self.api_url = api_url
         self.collection = collection
 
+    ########################
+    ### Helper Functions ###
+    ########################
+
+    def transform_bbox(
+        self, 
+        bbox: list, 
+        crs_src: str, 
+        crs_dst: str,
+    ) -> list:
+        """
+        Transform a bounding box to a desired CRS.
+
+        Parameters:
+        - crs_src (str): The source CRS.
+        - crs_dst (str): The destination CRS.
+
+        Returns:
+        - bbox_transformed (list): The transformed bounding box.
+        """
+        transformer = Transformer.from_crs(crs_src, crs_dst, always_xy=True)
+        bbox_transformed = [transformer.transform(x, y) for x, y in zip(bbox[::2], bbox[1::2])]
+
+        # flatten the list
+        bbox_transformed = [item for sublist in bbox_transformed for item in sublist]
+
+        return bbox_transformed
+    
+    def stretch_contrast(
+        self, 
+        stack: xarray.Dataset,
+    ) -> xarray.Dataset:
+        """
+        Stretch the contrast of a stackstac.stack object.
+
+        Parameters:
+        - stack (xarray.Dataset): The stackstac.stack object to stretch.
+
+        Returns:
+        - stack (xarray.Dataset): The contrast-stretched stackstac.stack object.
+        """
+        # Loop over each band in the stack
+        for band in range(len(stack)):
+
+            # Calculate the 2nd and 98th percentiles of the band data
+            min_val = int(stack[band].min().values)
+            max_val = int(stack[band].max().values)
+
+            # Stretch the contrast and scale the values to the range 0-255
+            stack[band] = 255 * (stack[band] - min_val) / (max_val - min_val)
+
+            # Clip the values to the range 0-255 and convert to uint8
+            stack[band] = stack[band].clip(0, 255).astype('uint8')
+
+        return stack
+
+    ############################
+    ### Reading STAC Methods ###
+    ############################
+
     def get_items(
         self,
         location: list, 
         buffer: int,
         timerange: str,
-        max_cloud_cover: int,
+        max_cloud_cover: int = 100,
     ) -> dict:
         """
         Query the STAC API. 
 
         Parameters: 
-        - location (list): The coordinates of the desired location, e.g. 
+        - location (list): Coordinates of desired location. Format [longitude, latitude], using WGS84.
         - buffer (int): The buffer size in kilometers.
         - timerange (str): The desired time range in the format "start_date/end_date".
         - max_cloud_cover (int): The maximum cloud cover percentage.
@@ -60,7 +118,6 @@ class ReadSTAC():
         Returns: 
         - items (dict): Dictionary containing all the found items meeting specified criteria. 
         """
-        
         location = Point(location)
         
         # Approximate degrees for 10 km buffer at the equator (1 degree of lat ~ 111 km at the equator)
@@ -102,16 +159,16 @@ class ReadSTAC():
         items = search.item_collection()
         print(f"{len(items)} Items found.")
 
-        return dict(items)
+        return items
+    
 
-
-    def load_item(
+    def filter_item(
         self, 
         items: dict, 
         filter_by: str,
     ) -> dict:
         """
-        Load a specific item from the STAC API. Currently supports loadinge eithe the most recent or the least cloudy item. 
+        Filter for a specific item from the STAC API. Currently supports loadinge eithe the most recent or the least cloudy item. 
         
         Parameters:
         - desired_item (str): one of ['most_recent', 'least_cloudy']. 
@@ -119,16 +176,15 @@ class ReadSTAC():
         Returns: 
         - item (dict): the metadata dictionary for the most recent or least cloudy item. 
         """
-
         #TODO: Currently this only works for single item/tile/orbit point. Need to implement for multiple tiles
         if filter_by == "most_recent":
             item = max(items, key=lambda item: item.datetime)
         elif filter_by == "least_cloudy":
             item = min(items, key=lambda item: eo.ext(item).cloud_cover)
         else:
-            item = None
-            print("Please specify either 'most_recent' or 'least_cloudy' as the desired item.")
-        
+            "No filter selected, returning all items."
+            return items
+
         print(
             f"Choosing {item.id} from {item.datetime.date()}"
             f" with {eo.ext(item).cloud_cover}% cloud cover"
@@ -142,7 +198,7 @@ class ReadSTAC():
         item: dict,
     ) -> tuple:
         """
-        Preview the item on an interactive map.
+        Preview an item on an interactive map.
         
         Parameters:
         - item (dict): The item to view, obtained from load_item().
@@ -151,37 +207,218 @@ class ReadSTAC():
         - Table: Table containing all the channels of the item
         - Cropped Image: preview of the image, cropped to the area of interest only
         """
-
-        table = rich.table.Table("Asset Key", "Description")
+        # Create dataframe with the channels and metadata
+        rows = []
         for asset_key, asset in item.assets.items():
-            table.add_row(asset_key, asset.title)
+            rows.append((asset_key, asset.title))
+        table = pd.DataFrame(rows, columns=["Asset Key", "Description"])
 
         # Rendering With Image Preview
-        image_url = item.assets["visual"].href
-        
-        with rasterio.open(image_url) as ds:
+        with rasterio.open(item.assets["visual"].href) as ds:
             aoi_bounds = features.bounds(self.bbox_geojson)
-            warped_aoi_bounds = warp.transform_bounds("epsg:4326", ds.crs, *aoi_bounds)
-            aoi_window = windows.from_bounds(transform=ds.transform, *warped_aoi_bounds)
-            band_data = ds.read(window=aoi_window)
+            warped_aoi_bounds = rasterio.warp.transform_bounds("epsg:4326", ds.crs, *aoi_bounds)
+            band_data = ds.read(window=rasterio.windows.from_bounds(transform=ds.transform, *warped_aoi_bounds))
 
         img = Image.fromarray(np.transpose(band_data, axes=[1, 2, 0]))
-        w = img.size[0]
-        h = img.size[1]
-        aspect = w / h
-        target_w = 800
-        target_h = (int)(target_w / aspect)
-        cropped_image = img.resize((target_w, target_h), Image.Resampling.BILINEAR)
+        aspect = img.size[0] / img.size[1]
+        cropped_image = img.resize((800, int(800 / aspect)), Image.Resampling.BILINEAR)
 
-        return(table, cropped_image)
+        return table, cropped_image
     
+    ############################################
+    ### Processing as xarray using stackstac ###
+    ############################################
+
+    def get_stack(
+        self, 
+        items: dict, 
+        filter_by: str = None,
+        resolution: int = 10,
+        bands: list = ['B02', 'B03', 'B04'],
+    ) -> xarray.DataArray:
+        """
+        Load a specific item from the STAC API using stackstac. 
+        Currently supports loading eithe the most recent or the least cloudy item. 
+        
+        Parameters:
+        - items (dict): Dictionary containing all the found items.
+        - filter_by (str, optional): one of ['most_recent', 'least_cloudy'].
+        - resolution (int, optional): The resolution of the image. Default is 10.
+        - bands (list, optional): The bands to load. Default is ['B02', 'B03', 'B04'].
+        - bounds (list, optional): The bounds of the image. Default is None. 
+
+        Returns: 
+        """
+        # Filter the items
+        item = self.filter_item(items, filter_by)
+
+        # slice the x and y dimensions to the specified bounds
+        # transform the bounds to the desired CRS
+        bounds = self.transform_bbox(self.bbox, "epsg:4326", item.properties["proj:epsg"])
+
+        # Load the item
+        stack = stackstac.stack(item, resolution=resolution, assets=bands, bounds=bounds)
+
+        # optionally removing the time dimension
+        stack = stack.squeeze()
+
+        return stack
+
+    def get_stretched_stack(
+        self, 
+        items, 
+        filter_by: str = None,
+        resolution: int = 10,
+        bands: list = ['B02', 'B03', 'B04'],
+    ) -> xarray.Dataset:
+        """
+        Get a stackstac.stack object, reproject it to a given CRS, and perform contrast stretching.
+
+        Parameters:
+        - items (dict): Dictionary containing all the found items.
+        - filter_by (str): One of ['most_recent', 'least_cloudy'].
+        - resolution (int): The resolution of the image.
+        - bands (list): The bands to load.
+        - bounds (list): The bounds of the image.
+        - target_crs (str): The target CRS.
+        - subsample (int, optional): The subsampling factor for contrast stretching. Default is 10.
+
+        Returns:
+        - stack_stretched (xarray.Dataset): The reprojected and contrast-stretched stackstac.stack object.
+        """
+        # Get the stack item
+        stack = self.get_stack(items, filter_by, resolution, bands)
+
+        # Perform contrast stretching
+        stack_stretched = self.stretch_contrast(stack)
+
+        return stack_stretched
+    
+    def save_stack_as_geotiff(
+        self, 
+        stack: xarray.Dataset, 
+        output_path: str,
+    ) -> None:
+        """
+        Save the stackstac.stack object as a GeoTIFF.
+
+        Parameters:
+        - stack (xarray.Dataset): The stackstac.stack object to save.
+        - output_path (str): The output path.
+
+        Returns:
+        - None
+        """
+        # Save the stack as a GeoTIFF
+        stack.rio.to_raster(output_path, compress="zstd")
+
+
+    def show_stack(
+        self, 
+        stack: xarray.Dataset,
+    ) -> None:
+        """
+        Display the stackstac.stack object.
+
+        Parameters:
+        - stack (xarray.Dataset): The stackstac.stack object to display.
+
+        Returns:
+        - None
+        """
+        # Plot the stack
+        stack.plot.imshow(robust=True, figsize=(10, 10))
+
+    #############################################
+    ### Downloading Whole Files from STAC API ###
+    #############################################
+
+    def download_band(
+        self,
+        item: dict, 
+        band_name: str, 
+        band_key: str,
+    ) -> None:
+        """
+        Download the specified band from the item to the specified path, in TIF format. 
+
+        Parameters:
+        - item (dict): The item to download, obtained from load_item().
+        - band_name (str): The name of the band.
+        - band_key (str): The key of the band.
+        - output_dir (str): The output directory, relative to the working directory /src.
+
+        Returns:
+        - None
+        """
+        # Get the directory of the current script
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+
+        # Set the output directory to 'src/data' relative to the script directory
+        output_dir = os.path.join(script_dir, 'data/temp')
+
+        # Create the output directory if it doesn't exist
+        os.makedirs(output_dir, exist_ok=True)
+
+        # Generate the full path where the file will be saved
+        file_path = os.path.join(output_dir, f"{item.id}_{band_name}.tif")
+
+        # Check if the file already exists
+        if os.path.exists(file_path):
+            print(f"File {file_path} already exists, skipping download.")
+            return
+        else:
+            print("Downloading", file_path)
+
+        # Get the asset/href for the specified band
+        asset = item.assets[band_key]
+
+        # Download the file
+        response = requests.get(asset.href)
+        if response.status_code == 200:
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+            print(f"Downloaded {file_path}")
+        else:
+            print(f"Failed to download {asset.href}")
+
+
+    def tif_to_vrt(
+            self,
+            item: dict, 
+            bands_to_download: dict,
+            output_dir: str,
+        ) -> None:
+            """
+            Create a VRT file for the specified bands.
+
+            Parameters:
+            - item (dict): The item to download, obtained from load_item().
+            - bands_to_download (dict): The bands to download, with band names as keys, and band keys as items.
+            - output_dir (str): The output directory.           
+            
+            Returns:
+            - None
+            """
+            item_id = item.id
+
+            # Loop through items and download the specified bands
+            for band_name, band_key in bands_to_download.items():
+                self.download_band(item, band_name, band_key, output_dir)
+
+            # Paths to the band files
+            band_paths = [str(f"{output_dir}/{item_id}_{band_name}.tif") for band_name in bands_to_download.keys()]
+            
+            vrt_filename = output_dir / f"{item_id}.vrt"
+            gdal.BuildVRT(str(vrt_filename), band_paths, separate=True, options=['COMPRESS=DEFLATE'])
+            print(f"Created VRT: {vrt_filename}")
+
 
     def download_item(
         self, 
         item: dict,
-        bands_to_download: dict, 
         output_dir: str,
-        output_format: str,
+        bands_to_download: dict = {'B02': 'blue', 'B03': 'green', 'B04': 'red'},
     ) -> str:
         """
         Download the desired bands.
@@ -193,140 +430,8 @@ class ReadSTAC():
         Returns: 
         - Path to the VRT 
         """
-        
-        def download_band(
-            item: dict, 
-            band_name: str, 
-            band_key: str,
-        ):
-            # Generate the full path where the file will be saved
-            file_path = output_dir / f"{item.id}_{band_name}.tif"
 
-            # Check if the file already exists
-            if file_path.exists():
-                print(f"File {file_path} already exists, skipping download.")
-                return
-
-            # Get the asset/href for the specified band
-            asset = item.assets[band_key]
-
-            # Download the file
-            response = requests.get(asset.href)
-            if response.status_code == 200:
-                with open(file_path, 'wb') as f:
-                    f.write(response.content)
-                print(f"Downloaded {file_path}")
-            else:
-                print(f"Failed to download {asset.href}")
-
-        def create_vrt(
-            item: dict, 
-            vrt_suffix: str,
-        ):
-            """
-            Create Virtual file pointing to the bands (RGB by default)
-
-            
-            """
-            item_id = item.id
-
-            # Loop through items and download the specified bands
-            for band_name, band_key in bands_to_download.items():
-                
-                download_band(item, band_name, band_key)
-                
-
-            
-            # Paths to the band files
-            red_band_path = output_dir / f"{item_id}_red.tif"
-            green_band_path = output_dir / f"{item_id}_green.tif"
-            blue_band_path = output_dir / f"{item_id}_blue.tif"
-            nir_band_path = output_dir / f"{item_id}_nir08.tif"
-            true_color_bands = [str(red_band_path), str(green_band_path), str(blue_band_path)]
-            
-            vrt_filename = output_dir / f"{item_id}_{vrt_suffix}.vrt"
-            gdal.BuildVRT(str(vrt_filename), band_files, separate=True, options=['COMPRESS=DEFLATE'])
-            print(f"Created VRT: {vrt_filename}")
-
-
-            
-        print("Bands Downloaded!")
-
-        create_vrt(item_id, true_color_bands, 'true_color')
-
-        print("VRT Created")
-        
-
-        
+        # Download the bands
+        create_vrt(item, bands_to_download, output_dir)
     
     
-
-    # enhance composites 
-    def stretch_contrast_with_subsampling(self, vrt_path, output_path, crop_bbox, subsample=10, compression='DEFLATE'):
-        
-        # Get the source dataset's projection from the first band file
-        src_ds = gdal.Open(str(vrt_path))
-        src_wkt = src_ds.GetProjection()
-        src_srs = osr.SpatialReference(wkt=src_wkt)
-
-        # Convert the osr SpatialReference to a pyproj CRS object
-        crs_src = CRS.from_wkt(src_wkt)
-
-        # Create transformer to project crop_bbox into source's CRS
-        transformer_to_src_crs = Transformer.from_crs("epsg:4326", crs_src, always_xy=True)
-
-        # Get bounds of crop_bbox in source's CRS
-        minx, miny, maxx, maxy = crop_bbox.bounds
-        minx, miny = transformer_to_src_crs.transform(minx, miny)
-        maxx, maxy = transformer_to_src_crs.transform(maxx, maxy)
-
-        # Crop VRT
-        cropped_vrt_filename = vrt_path.parent / f"tmp.vrt"
-        gdal.Warp(str(cropped_vrt_filename), str(vrt_path), outputBounds=(minx, miny, maxx, maxy), dstNodata=None)
-
-        # Open cropped VRT
-        src = gdal.Open(str(cropped_vrt_filename))
-        driver = gdal.GetDriverByName('GTiff')
-
-        # Define the creation options for compression
-        co_options = ['COMPRESS=' + compression]
-
-        dst_ds = None
-
-        for bi in range(1, src.RasterCount + 1):
-            band = src.GetRasterBand(bi)
-            band_arr_subsample = band.ReadAsArray(buf_xsize=band.XSize // subsample, buf_ysize=band.YSize // subsample)
-            b_min, b_max = np.percentile(band_arr_subsample, [2, 98])
-            band_arr = band.ReadAsArray()
-            band_arr = np.clip(255 * (band_arr - b_min) / (b_max - b_min), 0, 255).astype(np.uint8)
-
-            if dst_ds is None:
-                # Create the output dataset with compression options
-                dst_ds = driver.Create(str(output_path), src.RasterXSize, src.RasterYSize, src.RasterCount, gdal.GDT_Byte, options=co_options)
-                dst_ds.SetGeoTransform(src.GetGeoTransform())
-                dst_ds.SetProjection(src.GetProjection())
-
-            dst_ds.GetRasterBand(bi).WriteArray(band_arr)
-
-        dst_ds = None  # Close and save the dataset
-        cropped_vrt_filename.unlink()
-
-    
-    def download_and_stretch(self, item, bands: list = ['B02', 'B03', 'B04', 'B08']):
-        """
-        Download the desired bands from the item and stretch the contrast.
-        
-        Parameters:
-        - item (dict): The item to download, obtained from load_item().
-        - bands (list): The bands to download. Default is ['B02', 'B03', 'B04', 'B08'].
-        """
-        output_dir = self.download_item(item, bands)
-        band_files = list(output_dir.glob("*.tif"))
-
-        # Create VRT for the bands
-        self.create_vrt(item.id, band_files, "stacked")
-
-        # Stretch contrast with subsampling
-        for vrt_file in output_dir.glob("*.vrt"):
-            output_file = output_dir / f"{vrt_file.stem}.tif"
-            self.stretch_contrast_with_subsampling(vrt_file, output_file, self.bbox, subsample=10)
