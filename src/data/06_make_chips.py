983190197c0ea299d0a6e76174b3a84928a84d80
Simon Jasansky <s.jasansky@outlook.de>
2024-09-02 07:29:06 +0000

    bugfix: add chipping for test files, fix indentation bug of purge_chips

--- src/data/06_make_chips.py
+++ src/data/06_make_chips.py
@@ -95,42 +95,44 @@ def process_files(file_paths, output_dir, chip_size, chip_format):
         chip_format (str): Format to save the chips. Either 'npy' or 'tif'.
         must_contain_mining (bool): Flag to indicate if chips must contain some mining area.
     """
+    print(f"Processing files in {output_dir}")
     for file_path in tqdm(file_paths, desc="Processing files"):
         # print(f"Processing: {file_path}")
         read_and_chip(file_path, chip_size, output_dir, chip_format)
 
-        def purge_chips(chips_dir, labels_dir, chip_format):
-            """
-            Purges chips that do not contain mining area.
 
-            Args:
-                chips_dir (str or Path): Directory containing the chips.
-                labels_dir (str or Path): Directory containing the labels.
-                chip_format (str): Format of the chips.
-            """
-            print(f"Purging chips in {chips_dir} that do not contain mining area")
-            removed_count = 0
-            chips = list(chips_dir.glob(f"*.{chip_format}"))
-            for chip_path in tqdm(chips, desc="Purging chips"):
-                label_path = labels_dir / f"{chip_path.stem}.{chip_format}"
-                label_path = str(label_path).replace("_img", "_mask")
+def purge_chips(chips_dir, labels_dir, chip_format):
+    """
+    Purges chips that do not contain mining area.
+
+    Args:
+        chips_dir (str or Path): Directory containing the chips.
+        labels_dir (str or Path): Directory containing the labels.
+        chip_format (str): Format of the chips.
+    """
+    print(f"Purging chips in {chips_dir} that do not contain mining area")
+    removed_count = 0
+    chips = list(chips_dir.glob(f"*.{chip_format}"))
+    for chip_path in tqdm(chips, desc="Purging chips"):
+        label_path = labels_dir / f"{chip_path.stem}.{chip_format}"
+        label_path = str(label_path).replace("_img", "_mask")
+        
+        if chip_format == "npy":
+            label = np.load(label_path)
+            if np.sum(label) == 0:
+                os.remove(chip_path)
+                os.remove(label_path)
+                removed_count += 1
                 
-                if chip_format == "npy":
-                    label = np.load(label_path)
-                    if np.sum(label) == 0:
-                        os.remove(chip_path)
-                        os.remove(label_path)
-                        removed_count += 1
-                        
-                if chip_format == "tif":
-                    with rio.open(label_path) as src:
-                        label = src.read()
-                        if np.sum(label) == 0:
-                            os.remove(chip_path)
-                            os.remove(label_path)
-                            removed_count += 1
-            
-            print(f"Removed {removed_count} chips")
+        if chip_format == "tif":
+            with rio.open(label_path) as src:
+                label = src.read()
+                if np.sum(label) == 0:
+                    os.remove(chip_path)
+                    os.remove(label_path)
+                    removed_count += 1
+    
+    print(f"Removed {removed_count} chips")
 
 def main():
     """
@@ -168,8 +170,10 @@ def main():
 
     train_image_paths = list((data_dir / "train").glob("*_img.tif"))
     val_image_paths = list((data_dir / "val").glob("*_img.tif"))
+    test_image_paths = list((data_dir / "test").glob("*_img.tif"))
     train_label_paths = list((data_dir / "train").glob("*_mask.tif"))
     val_label_paths = list((data_dir / "val").glob("*_mask.tif"))
+    test_label_paths = list((data_dir / "test").glob("*_mask.tif"))
 
     # check if chips already exist and remove them
     if output_dir.exists():
@@ -178,13 +182,16 @@ def main():
     
     process_files(train_image_paths, output_dir / "train/chips", chip_size, chip_format)
     process_files(val_image_paths, output_dir / "val/chips", chip_size, chip_format)
+    process_files(test_image_paths, output_dir / "test/chips", chip_size, chip_format)
     process_files(train_label_paths, output_dir / "train/labels", chip_size, chip_format)
     process_files(val_label_paths, output_dir / "val/labels", chip_size, chip_format)
+    process_files(test_label_paths, output_dir / "test/labels", chip_size, chip_format)
 
     if must_contain_mining:
         print("Purging chips that do not contain mining area")
         purge_chips(output_dir / "train/chips", output_dir / "train/labels", chip_format)
         purge_chips(output_dir / "val/chips", output_dir / "val/labels", chip_format)
+        purge_chips(output_dir / "test/chips", output_dir / "test/labels", chip_format)
     
     if normalize and chip_format == "tif":
         print("Normalizing chips")
@@ -192,10 +199,13 @@ def main():
             normalize_geotiff(chip_path, chip_path)
         for chip_path in (output_dir / "val" / "chips").glob(f"*.{chip_format}"):
             normalize_geotiff(chip_path, chip_path)
+        for chip_path in (output_dir / "test" / "chips").glob(f"*.{chip_format}"):
+            normalize_geotiff(chip_path, chip_path)
 
     print(f"Chips saved to {output_dir}")
     print(f"Nr. Train chips: {len(list((output_dir / 'train' / 'chips').glob(f'*.{chip_format}')))}")
     print(f"Nr. Val chips: {len(list((output_dir / 'val' / 'chips').glob(f'*.{chip_format}')))}")
+    print(f"Nr. Test chips: {len(list((output_dir / 'test' / 'chips').glob(f'*.{chip_format}')))}")
 
 if __name__ == "__main__":
     main()
