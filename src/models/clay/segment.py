542de7ec9e42ff181a5d73050d1b4235fc63c00e
Simon Jasansky <s.jasansky@outlook.de>
2024-09-05 12:03:35 +0200

    feat: make data augmentation optional in config file

--- src/models/clay/segment.py
+++ src/models/clay/segment.py
@@ -9,7 +9,6 @@ References:
 - https://lightning.ai/docs/pytorch/2.1.0/cli/lightning_cli.html
 - https://pytorch-lightning.medium.com/introducing-lightningcli-v2-supercharge-your-training-c070d43c7dd6
 """
-import argparse 
 
 from lightning.pytorch.cli import LightningCLI
 
@@ -22,20 +21,7 @@ def cli_main():
     """
     Command-line inteface to run ClayMAE with ClayDataModule.
     """
-    parser = argparse.ArgumentParser(description="Train the neural network model.")
-    parser.add_argument('--data_augmentation', action='store_true', help="Apply data augmentation.")
-    args = parser.parse_args()
-
-    cli = LightningCLI(
-        MineSegmentor,
-        MineDataModule,
-        save_config_kwargs={"overwrite": True},
-        run=False
-    )
-
-    # Update datamodule with flip argument
-    cli.datamodule.data_augmentation = args.data_augmentation
-    cli.trainer.fit(cli.model, datamodule=cli.datamodule)
+    cli = LightningCLI(MineSegmentor, MineDataModule, save_config_kwargs={"overwrite": True})
     return cli
 
 
